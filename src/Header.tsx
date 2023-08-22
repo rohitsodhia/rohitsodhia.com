e6696dbc8eec0e3a9cdcc3b4e5ea115b58aa7f17
Rohit Sodhia <sodhia.rohit@gmail.com>
2023-08-21 21:28:14 -0400

    Finish header

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,11 +1,24 @@
+import { useResumeContext } from "./ResumeContext";
+
 export default function Header() {
+    const resume = useResumeContext();
+
     return (
-        <div className="flex">
+        <div className="md:flex">
             <div>
                 <h1 className="text-3xl font-bold">Rohit Sodhia</h1>
-                <h2 className="text-xl mt-1">Software Engineer</h2>
+                <div className="text-xl mt-1">Software Engineer</div>
             </div>
-            <div className="md:flex md:justify-self-end"></div>
+            <div className="mt-2 md:flex md:justify-self-end font-bold">
+                <div>{resume?.location}</div>
+                <div></div>
+            </div>
+            <div className="mt-3">
+                Full stack software engineer, dominantly in backend
+                technologies. Experienced with full product life-cycle
+                development, from design to support, and helping organize and
+                lead engineering teams.
+            </div>
         </div>
     );
 }
